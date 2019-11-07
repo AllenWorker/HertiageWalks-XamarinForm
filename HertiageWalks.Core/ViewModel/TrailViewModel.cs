8846020b257af0b625dcc1dc5d2e475a46b5ddf8
Allen <allen88038@gmail.com>
2019-11-08 00:25:46 +0800

    Fix TrailViewModel and Binding to MainView

--- HertiageWalks.Core/ViewModel/TrailViewModel.cs
+++ HertiageWalks.Core/ViewModel/TrailViewModel.cs
@@ -10,7 +10,7 @@ namespace HertiageWalks.Core.ViewModel
 {
     public class TrailViewModel : BaseViewModel
     {
-        private Trail trail;
+        
         private List<Trail> trails;
         public HeritageWalkService HeritageWalkService { get; } = new HeritageWalkService();
 
@@ -21,35 +21,9 @@ namespace HertiageWalks.Core.ViewModel
             
         }
 
-        public int TrailID
-        {
-            get { return trail.id; }
-            set { OnPropertyChanged(); }
-        }
+        
 
-        public string TrailName
-        {
-            get { return trail.name; }
-            set { OnPropertyChanged(); }
-        }
-
-        public string TrailImage
-        {
-            get { return trail.img; }
-            set { OnPropertyChanged(); }
-        }
-
-        public string TrailDistance
-        {
-            get { return trail.length; }
-            set { OnPropertyChanged(); }
-        }
-
-        public string TrailTime
-        {
-            get { return trail.time; }
-            set { OnPropertyChanged(); }
-        }
+        
 
         public List<Trail> Trails 
         {
@@ -60,8 +34,7 @@ namespace HertiageWalks.Core.ViewModel
         public async void LoadData()
         {
             trails = await HeritageWalkService.GetAllTrails();
-            trail = trails[0];
-            OnPropertyChanged();
+            OnPropertyChanged("Trails");
         }
         
     }
